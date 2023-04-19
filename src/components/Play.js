415ee79b745dbbf78fd315f208b588b7a1641279
nicklitvin <nick.litvinov9@gmail.com>
2023-04-18 21:21:04 -0700

    message

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,7 +1,11 @@
+import styles from "../css/Play.module.css";
+
 function Play() {
   return (
     <div>
-      <p>Play</p>
+      <p className={styles.playText}>Play</p>
+      <h1>HI</h1>
+      <button>tea</button>
     </div>
   );
 }
